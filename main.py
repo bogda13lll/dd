687a0c033021724a5a2f696fce2eaff93e82dff2
Bohdan git config --global user.email <b.bondar2013@gmail.com>
2026-02-03 20:12:46 +0200

    added main game

--- main.py
+++ main.py
@@ -21,3 +21,25 @@ for i in range(attempts):
         print("Більше")
 if guess != secret_number:
     print("Спроби закінчилися. Було число " + str(secret_number))
+    start = int(input("Введіть число з якого починати: "))
+end = int(input("Введіть число по яке виводити: "))
+print("Список чисел від " + str(start) + " до " + str(end) + ":")
+for i in range(start, end + 1):
+    print(i)
+n = int(input("Введіть число n: "))
+print("Парні числа від " + str(n) + " до 1 у зворотному порядку:")
+for i in range(n, 0, -1):
+    if i % 2 == 0: 
+        print(i, end=" ") 
+        score = int(input("Введіть кількість балів (0-100): "))
+
+if score >= 0 and score <= 49:
+    print("Незадовільно")
+elif score >= 50 and score <= 69:
+    print("Задовільно")
+elif score >= 70 and score <= 89:
+    print("Добре")
+elif score >= 90 and score <= 100:
+    print("Відмінно")
+else:
+    print("Помилка: введіть число від 0 до 100")

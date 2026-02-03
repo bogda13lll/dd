e4e6d5a82885b0a66e77a613cccedd3d55b72ba5
Bohdan git config --global user.email <b.bondar2013@gmail.com>
2026-02-03 20:00:08 +0200

    Додав код гри та виправив імпорт

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-echo "# dd" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/bogda13lll/dd.git
-git push -u origin main
+import random 
+name = input("Введіть ваше ім'я: ")
+age_input = input("Введіть ваш вік: ")
+age = int(age_input)
+print("Привіт {}, тобі {}!".format(name, age))
+if age > 18:
+    print("Вхід дозволено!")
+else:
+    print("Вхід заборонено!")
+secret_number = random.randint(1, 10)
+attempts = 3
+print("Я загадав число від 1 до 10. У тебе є 3 спроби!")
+for i in range(attempts):
+    guess = int(input("Спроба №" + str(i + 1) + ". Введи число: "))
+    if guess == secret_number:
+        print("Вгадав!")
+        break
+    elif guess > secret_number:
+        print("Менше")
+    else:
+        print("Більше")
+if guess != secret_number:
+    print("Спроби закінчилися. Було число " + str(secret_number))
